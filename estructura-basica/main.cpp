8c86da8b28947e4a16cb7dda97c0b033bfe5fcfc
jzlbornoz <javieraap10@gmail.com>
2022-05-20 20:34:03 -0400

    Salida y entrada de Datos

--- estructura-basica/main.cpp
+++ estructura-basica/main.cpp
@@ -5,10 +5,7 @@ using namespace std;
 int main()
 {
     int edad = 0;
-    edad = 23;
-    const char nombre = 'j';
-    int noteList[] = {20, 12, 16, 12, 18};
-    cout << edad << endl;
-    cout << nombre << endl;
-    cout << noteList[2] << endl;
+    cout << "edad: +" << endl; //endl funciona como br en html
+    cin >> edad;
+    cout << "tu valor es: " << edad << endl;
 }
