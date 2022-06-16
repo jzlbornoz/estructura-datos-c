fe2bc9787767ceb88bed9c231029143de7f8b18a
jzlbornoz <javieraap10@gmail.com>
2022-06-15 22:13:29 -0400

    Se corrigio error en el ejercicio

--- exercises/aumento-salario/main.cpp
+++ exercises/aumento-salario/main.cpp
@@ -3,20 +3,26 @@
 
 using namespace std;
 
-int main () {
+int main()
+{
     system("clear");
     float sueldo;
     cout << "===INGRESA TU SUELDO===" << endl;
     cin >> sueldo;
-    if (sueldo <= 200){
+    if (sueldo <= 200)
+    {
         float porcentajeUP = sueldo / 2;
         float nuevoSueldo = sueldo + porcentajeUP;
         cout << "Debes reclamar un aumento del 50%, tu nuevo sueldo sera de: " << nuevoSueldo << endl;
-    }else if (sueldo >= 200 && sueldo <=500){
+    }
+    else if (sueldo >= 200 && sueldo <= 500)
+    {
         float porcentajeUp = sueldo / 5;
         float nuevoSalario = sueldo + porcentajeUp;
         cout << "Debes reclamar un aumento del 20%, tu nuevo sueldo sera de: " << nuevoSalario << endl;
-    } else {
-            cout << "Tienes un buen sueldo, no necesitas un aumento" << endl;
+    }
+    else
+    {
+        cout << "Tienes un buen sueldo, no necesitas un aumento" << endl;
     }
 }
